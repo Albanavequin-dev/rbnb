4fd69d94ebb049767e01956789f80a094fe150d8
Mathis Desbriere <mathis.desbriere@hotmail.fr>
2023-05-30 11:29:21 +1000

    Bug resolve (presence missing in validation)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :bookings
   validates :email, uniqueness: true, presence: true
   validates :first_name, presence: true
-  validates :last_name, true
+  validates :last_name, presence: true
   validates :password, length: { minimum: 6 }
 end
