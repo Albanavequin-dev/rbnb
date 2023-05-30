3dc945b475f0f5e57fbcb1ec1d6c4155e242465f
Mathis Desbriere <mathis.desbriere@hotmail.fr>
2023-05-30 16:01:25 +1000

    Flats controller + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "flats#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :flats
-  resources :bookings
+  resources :flats do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
 end
