d979e433a95fcbb5bb590d4624ead3cde258142a
Mathis Desbriere <mathis.desbriere@hotmail.fr>
2023-05-30 17:17:25 +1000

    Improve of authorizations

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -5,4 +5,28 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
