6cc0c4256c87d441715d3a8a48e5e8286c002a9d
alban avequin <avequin.alban@icloud.com>
2023-05-31 16:20:48 +1000

    flat are displayed on the index

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,11 +2,9 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:show, :index]
 
-
   def index
     @flats = Flat.all
     @flats = policy_scope(Flat)
-    @flats = Flat.all
   end
 
   def show
@@ -20,31 +18,31 @@ class FlatsController < ApplicationController
 
   def create
     @flat = Flat.new(flat_params)
-    @flat.save
-    redirect_to flat_path(@flat)
-    authorize @flat #line must be at the end of the method WARNING
+    @flat.user = current_user
+    authorize @flat
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
     authorize @flat #line must be at the end of the method WARNING
   end
-
   def update
     @flat.update(flat_params)
     redirect_to flat_path(@flat)
     authorize @flat #line must be at the end of the method WARNING
   end
-
   def destroy
     @flat.destroy
     redirect_to restaurant_path, status: :see_other
     authorize @flat #line must be at the end of the method WARNING
   end
-
   def flat_params
-    params.require(:flat).permit(:address, :description, :wifi, :TV, :parking, :air_conditionner)
+    params.require(:flat).permit(:address, :description, :wifi, :TV, :parking, :air_conditioner)
   end
-
   def set_flat
     @flat = Flat.find(params[:id])
   end
