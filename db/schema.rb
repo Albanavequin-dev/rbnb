94884e612483b9daea983d81717d3d7163a78cd4
alban avequin <avequin.alban@icloud.com>
2023-06-01 11:15:33 +1000

    fixing bugs with seeds

--- db/schema.rb
+++ db/schema.rb
@@ -57,8 +57,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_003315) do
   end
 
   create_table "flats", force: :cascade do |t|
-    t.string "description"
+    t.string "name"
     t.string "address"
+    t.string "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -66,7 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_003315) do
     t.boolean "TV"
     t.boolean "parking"
     t.boolean "air_conditioner"
-    t.string "name"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
