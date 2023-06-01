768e8bdc926e923f43ed05f2ab5975e1106b8050
Mathis Desbriere <mathis.desbriere@hotmail.fr>
2023-06-01 11:12:33 +1000

    Adding stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import BookingsController from "./bookings_controller"
+application.register("bookings", BookingsController)
